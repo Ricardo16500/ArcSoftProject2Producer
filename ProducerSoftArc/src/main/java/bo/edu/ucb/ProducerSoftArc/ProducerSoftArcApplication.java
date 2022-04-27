a4391803bc75b4e8c0093d8f8a16c58a7b2e275e
Ricardo16500 <fabiorvm16500@gmail.com>
2022-04-26 21:48:36 -0400

    agregando el proyecto spring dentro del repo

---
+++ ProducerSoftArc/src/main/java/bo/edu/ucb/ProducerSoftArc/ProducerSoftArcApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ProducerSoftArc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerSoftArcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerSoftArcApplication.class, args);
+	}
+
+}
---
+++ ProducerSoftArc/src/test/java/bo/edu/ucb/ProducerSoftArc/ProducerSoftArcApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ProducerSoftArc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerSoftArcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
